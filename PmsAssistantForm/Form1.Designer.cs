625714e9c4131ea26a0a6b5d109809120f5f80cd
dada <dada@dada.com>
2017-04-22 18:12:55 +0800

    *

--- PmsAssistantForm/Form1.Designer.cs
+++ PmsAssistantForm/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.wBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wBrowser
+            // 
+            this.wBrowser.Location = new System.Drawing.Point(735, 12);
+            this.wBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wBrowser.Name = "wBrowser";
+            this.wBrowser.Size = new System.Drawing.Size(201, 165);
+            this.wBrowser.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 530);
+            this.Controls.Add(this.wBrowser);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser wBrowser;
     }
 }
 
